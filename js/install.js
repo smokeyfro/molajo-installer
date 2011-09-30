45a27a518b43c53b33d20f3040de201dd2243357
Chris Rault <chris@prothemer.com>
2011-09-30 16:15:18 +0200

    Code comments and inline labels js

--- js/install.js
+++ js/install.js
@@ -1,13 +1,37 @@
 $(function() {
+	// Toggle the languages drop down
 	$("#select-language").click(function(){ 
 		$(this).toggleClass("active");
 		$("#language ul").toggle();
 		return false;
 	});
 	
+	// Switch the selected language text and class
 	var langClass;
 	$("#language li a").click(function () {
 		langClass = $(this).parent().attr("class");
 		$('#language span strong').replaceWith( '<strong class="'+langClass+'">' + $(this).text() + '</strong>' );
+	});
+	
+	// Inline labels
+	$("label.inlined + input.input-text").each(function (type) {
+		Event.observe(window, 'load', function () {
+			setTimeout(function(){
+				if (!input.value.empty()) {
+					input.previous().addClassName('has-text');
+				}
+			}, 200);
+		});
+		$(this).focus(function () {
+			$(this).prev("label.inlined").addClass("focus");
+		});
+		$(this).keypress(function () {
+			$(this).prev("label.inlined").addClass("has-text").removeClass("focus");
+		});
+		$(this).blur(function () {
+			if($(this).val() == "") {
+				$(this).prev("label.inlined").removeClass("has-text").removeClass("focus");
+			}
+		});
 	});
 });

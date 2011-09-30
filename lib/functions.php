c2f0bf3ff97cd2537580a0c1295807fe668dd8c9
Chris Rault <chris@prothemer.com>
2011-09-30 15:55:30 +0200

    Wrapping PIE script for IE only.

--- lib/functions.php
+++ lib/functions.php
@@ -1,12 +1,13 @@
 <?php
 include_once('browser.php');
-include_once('PIE.php');
-
-$browser 					= new MBrowser();
-$thebrowser 				= preg_replace("/[^A-Za-z]/i", "", $browser->getBrowser());
-$ver 						= $browser->getVersion();
-$dots 						= ".";
-$dashes 					= "";
-$mod_chrome					= "";
-$ver 						= str_replace($dots , $dashes , $ver);
-$lcbrowser 					= strtolower($thebrowser);
+if ($lcbrowser == 'internetexplorer'){ 
+	include_once('PIE.php');
+}
+$browser 	= new MBrowser();
+$thebrowser = preg_replace("/[^A-Za-z]/i", "", $browser->getBrowser());
+$ver 		= $browser->getVersion();
+$dots 		= ".";
+$dashes 	= "";
+$mod_chrome	= "";
+$ver 		= str_replace($dots , $dashes , $ver);
+$lcbrowser 	= strtolower($thebrowser);

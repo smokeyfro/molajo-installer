dbdab182c92f7a201e15f58010f46a8eddec9d00
Chris Rault <chris@prothemer.com>
2011-09-30 15:58:14 +0200

    Booyah! *Pats self on back* (its the small things in life..)

--- js/install.js
+++ js/install.js
@@ -1,7 +1,13 @@
 $(function() {
 	$("#select-language").click(function(){ 
 		$(this).toggleClass("active");
-		$("#languages").toggle();
+		$("#language ul").toggle();
 		return false;
+	});
+	
+	var langClass;
+	$("#language li a").click(function () {
+		langClass = $(this).parent().attr("class");
+		$('#language span strong').replaceWith( '<strong class="'+langClass+'">' + $(this).text() + '</strong>' );
 	});
 });

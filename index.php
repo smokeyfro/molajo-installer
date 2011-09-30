98fd3614724d260bc806ddba3b40b1e2f34ae7d8
Chris Rault <chris@prothemer.com>
2011-09-30 15:56:11 +0200

    Shifting code..

--- index.php
+++ index.php
@@ -28,22 +28,22 @@
 								<li class="en-uk"><a href="#">UK English</a></li>
 								<li class="de-de"><a href="#">German</a></li>
 								<li class="it-it"><a href="#">Italian</a></li>
-								<li class="po-br"><a href="#">Brazilian Portugues</a></li>
+								<li class="po-br"><a href="#">Brazilian Portuguese</a></li>
 							</ul>
 						</span>
 					</li>
-					<!--<li id="version" class="valid">
+					<li id="version" class="valid">
 						<span>You are installing the latest version of Molajo.</span>
-					</li>-->
-					<li id="version" class="invalid">
+					</li>
+					<!--<li id="version" class="invalid">
 						<span>It seems you are installing an outdated version of Molajo. Please update before installing. <a href="#" id="update-installer"><span>Update Installer</span></a></span>
-					</li>
-					<!--<li id="requirements" class="valid">
-						<span>Your server meets all of the system requirements.</span>
 					</li>-->
-					<li id="requirements" class="invalid">
-						<span>There seems to an issue with your server configuration. Please address the issues listed below  <a href="#" id="check-requirements"><span>Check Again</span></a></span>
+					<li id="requirements" class="valid">
+						<span>Your server meets all of the system requirements.</span>
 					</li>
+					<!--<li id="requirements" class="invalid">
+						<span>There seems to an issue with your server configuration. Please address the issues listed below  <a href="#" id="check-requirements"><span>Check Again</span></a></span>
+					</li>-->
 				</ul>
 				<div id="actions">
 					<a href="#" class="btn-primary"><strong>Ready?</strong> Lets get started! &raquo;</a>
